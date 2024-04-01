9766c1d0a8113f3e02367eeff34cc7374a690c79
pedropelaes <pedro.pm2@puccampinas.edu.br>
2024-04-01 18:00:33 -0300

    2°Modificação da fase 1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Repositório do projeto integrador do curso de engenharia de software PUC Campinas 2024. Como projeto desenvolveremos um sistema de cadastro de produtos que que possa calcular o 
 preço de venda de um produto e ajudar o usuário a controlar o lucro de seu negócio, por meio da linguagem python. 
+
+Bibliotecas Utilizadas:
+Pandas:https://pypi.org/project/pandas/
+Tabulate:https://pypi.org/project/tabulate/
--- projeto_integrador.py
+++ projeto_integrador.py
@@ -1,3 +1,7 @@
+import pandas as pd 
+from tabulate import tabulate
+
+#Leitura dos dados:
 cod_prod=int(input("Digite o código do produto: "))
 nome_prod=str(input("Digite o nome do produto: "))
 descrição=str(input("Descreva o produto: "))
@@ -7,9 +11,44 @@ CV=float(input("Digite a comissão de venda: "))
 IV=float(input("Digite os impostos sobre a venda: "))
 ML=float(input("Digite a margem de lucro desejada: "))
 
+#Calculo do preço de venda:
 PV = CP/(1-((CF+CV+IV+ML)/100))
-print(f"{PV:.2f}")
+#################################
 
+
+#Custo de aquisição:
+PCA=(CP*100)//PV
+#Receita bruta:
+RB=PV-CP
+PRB=100-PCA
+#Custofixo:
+PCF=(CF*PRB)//RB
+#Comissão
+VCV=(CV*RB)//PRB
+#ValorImposto
+VI=(IV*RB)//PRB
+#OutrosCustos
+OC=CF+VCV+VI
+POC=(OC*PRB)//RB
+#Rentabilidade
+R=RB-OC
+
+
+tabela = [
+    ["Descrição", "Valor", "%"],
+    ["A.Preço de Venda:", PV, "100"],
+    ["B.Custo de Aquisição:", CP, int(PCA)],
+    ["C.Receita Bruta(A-B):", RB, int(PRB)],
+    ["D.Custo Fixo/Administrativo:", CF, int(PCF)],
+    ["E.Comissão de vendas:", VCV, int(CV)],
+    ["F.Impostos:", VI, int(IV)],
+    ["G.Outros Custos(D+E+F):", OC, int(POC)],
+    ["H.Rentabilidade(C-G):", R, int(ML)]
+]
+
+print(tabulate(tabela, headers="firstrow", tablefmt="rounded_outline", floatfmt=".2f"))
+
+#Classificação de lucro:
 if ML>20:
     print("Lucro alto")
 elif ML>10 and ML<=20:
@@ -20,3 +59,4 @@ elif ML==0:
     print("Equilibrio")
 elif ML<0:
     print("Prejuízo")
+######################################
