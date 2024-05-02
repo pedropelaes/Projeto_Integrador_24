43b106ed2eee05d9593951f90a4d8cbdcdfa5cac
pedromalinconico <pedro.pm2@puccampinas.edu.br>
2024-05-02 16:42:15 -0300

    início da 3ºfase

--- projeto_integrador.py
+++ projeto_integrador.py
@@ -2,105 +2,189 @@ from tabulate import tabulate
 import oracledb
 import getpass
 
-#conexão ao banco de dados
 userpwd = getpass.getpass("Enter password: ")
-connection = oracledb.connect(user="PEDROMALINCONICO", password=userpwd,
-                              host="localhost", port=1521, service_name="XEPDB1")
-cursor = connection.cursor()
+sel1=0
+while sel1 != 5:
+    
+    #conexão ao banco de dados
+    
+    connection = oracledb.connect(user="PEDROMALINCONICO", password=userpwd,
+                                host="localhost", port=1521, service_name="XEPDB1")
+    cursor = connection.cursor()
 
 
-#Leitura dos dados:
-cod_prod=int(input("Digite o código do produto: "))
-nome_prod=str(input("Digite o nome do produto: "))
-descricao=str(input("Descreva o produto: "))
-CP=float(input("Digite o custo do produto: "))
-CF=float(input("Digite o custo fixo/adiministrativo: "))
-CV=float(input("Digite a comissão de venda: "))
-IV=float(input("Digite os impostos sobre a venda: "))
-ML=float(input("Digite a margem de lucro desejada: "))
-
-#Calculo do preço de venda:
-soma=CF+CV+IV+ML
-if soma>100:
-    soma = soma * -1
-
-PV = CP/(1-((soma)/100))
-#################################
+    #Menu7
+    sel1=int(input("Digite: \n 1-Inserir Produto \n 2-Alterar Produto \n 3-Apagar Produto \n 4-Listar Produtos \n 5-Sair \n"))
 
 
-#Custo de aquisição:
-PCA=(CP*100)//PV
-#Receita bruta:
-RB=PV-CP
-PRB=100-PCA
-#ValorImposto
-VI=(IV/100)*PV
-#Comissão
-VCV=(CV/100)*PV
-#Rentabilidade
-R=(ML/100)*PV
-#Custofixo:
-VCF=(CF/100)*PV
-#OutrosCustos
-OC=VCF+VCV+VI
-POC=CF+CV+IV
+    if sel1 == 1:
+        #Leitura dos dados:
+        cod_prod=int(input("Digite o código do produto: "))
+        nome_prod=str(input("Digite o nome do produto: "))
+        descricao=str(input("Descreva o produto: "))
+        CP=float(input("Digite o custo do produto(R$): "))
+        CF=float(input("Digite o custo fixo/adiministrativo(%): "))
+        CV=float(input("Digite a comissão de venda(%): "))
+        IV=float(input("Digite os impostos sobre a venda(%): "))
+        ML=float(input("Digite a margem de lucro desejada(%): "))
 
-#organização e impressão da tabela
-tabela = [
-    ["Descrição", "Valor", "%"],
-    ["A.Preço de Venda:", PV, "100%"],
-    ["B.Custo de Aquisição:", CP, f"{PCA}%"],
-    ["C.Receita Bruta(A-B):", RB, f"{PRB}%"],
-    ["D.Custo Fixo/Administrativo:", VCF, f"{CF}%"],
-    ["E.Comissão de vendas:", VCV, f"{CV}%"],
-    ["F.Impostos:", VI, f"{IV}%"],
-    ["G.Outros Custos(D+E+F):", OC, f"{POC}%"],
-    ["H.Rentabilidade(C-G):", R, f"{ML}%"]
-]
+        #Calculo do preço de venda:
+        soma=CF+CV+IV+ML
+        if soma>100:
+            soma = soma * -1
 
-print(tabulate(tabela, headers="firstrow", tablefmt="rounded_outline", floatfmt=".2f"))
-
-#Classificação de lucro:
-if ML>20:
-    print("Lucro alto")
-elif ML>10 and ML<=20:
-    print("Lucro médio")
-elif ML>0 and ML<=10:
-    print("Lucro baixo")
-elif ML==0:
-    print("Equilibrio")
-elif ML<0:
-    print("Prejuízo")
-######################################
-
-#Inserção dos valores no banco de dados
-dados = [
-    (cod_prod, nome_prod, descricao, CP, VCF, VCV, VI, R),
-]
-
-cursor.executemany("INSERT INTO Projeto_Integrador (COD_PROD, NOME_PROD, DESCRICAO, CP, CF, CV, IV, ML) VALUES (:1, :2, :3, :4, :5, :6, :7, :8)", dados)
-print("Valores inseridos no banco de dados")
-connection.commit()
-
-#Visualização do banco de dados
-cursor.execute("SELECT * FROM Projeto_Integrador")
-visualização = cursor.fetchall()
-
-for fileira in visualização:
-    print(f"{fileira}")
-
-#apagar dados
-select = input("1-Truncate; 2-apagar; 3-Editar;: ")
-if select == '1':
-    cursor.execute("TRUNCATE TABLE Projeto_Integrador")
-elif select == '2':
-    código = input("Digite o código do produto que deseja apagar: ")
-    deletar = "DELETE FROM Projeto_Integrador WHERE COD_PROD = :código"
-    cursor.execute(deletar, código=código)
-    connection.commit()
+        PV = CP/(1-((soma)/100))
+        #################################
 
 
+        #Custo de aquisição:
+        PCA=(CP*100)//PV
+        #Receita bruta:
+        RB=PV-CP
+        PRB=100-PCA
+        #ValorImposto
+        VI=(IV/100)*PV
+        #Comissão
+        VCV=(CV/100)*PV
+        #Rentabilidade
+        R=(ML/100)*PV
+        #Custofixo:
+        VCF=(CF/100)*PV
+        #OutrosCustos
+        OC=VCF+VCV+VI
+        POC=CF+CV+IV
+
+        #organização e impressão da tabela
+        tabela = [
+            ["Descrição", "Valor", "%"],
+            ["A.Preço de Venda:", PV, "100%"],
+            ["B.Custo de Aquisição:", CP, f"{PCA}%"],
+            ["C.Receita Bruta(A-B):", RB, f"{PRB}%"],
+            ["D.Custo Fixo/Administrativo:", VCF, f"{CF}%"],
+            ["E.Comissão de vendas:", VCV, f"{CV}%"],
+            ["F.Impostos:", VI, f"{IV}%"],
+            ["G.Outros Custos(D+E+F):", OC, f"{POC}%"],
+            ["H.Rentabilidade(C-G):", R, f"{ML}%"]
+        ]
+
+        print(tabulate(tabela, headers="firstrow", tablefmt="rounded_outline", floatfmt=".2f"))
+
+        #Classificação de lucro:
+        if ML>20:
+            print(tabulate([["Lucro alto"]], tablefmt="rounded_outline"))
+            print()
+        elif ML>10 and ML<=20:
+            print(tabulate([["Lucro médio"]], tablefmt="rounded_outline"))
+            print()
+        elif ML>0 and ML<=10:
+            print(tabulate([["Lucro baixo"]], tablefmt="rounded_outline"))
+            print()
+        elif ML==0:
+            print(tabulate([["Equilibro"]], tablefmt="rounded_outline"))
+            print()
+        elif ML<0:
+            print(tabulate([["Prejuízo"]], tablefmt="rounded_outline"))
+            print()
+        ######################################
+
+        #Inserção dos valores no banco de dados
+        dados = [
+            (cod_prod, nome_prod, descricao, CP, CF, CV, IV, ML),
+        ]
+
+        cursor.executemany("INSERT INTO Projeto_Integrador (COD_PROD, NOME_PROD, DESCRICAO, CP, CF, CV, IV, ML) VALUES (:1, :2, :3, :4, :5, :6, :7, :8)", dados)
+        print("Valores inseridos no banco de dados")
+        connection.commit()
+
+    if sel1 == 4:
+        #seleção dos itens na tabela
+        lista=[]
+        cursor.execute("select * from Projeto_Integrador")
+        while True:
+            row = cursor.fetchone()
+            if row is None:
+                break
+            lista.append(row)
+ #(no sql) 0-Cod 1-Nome 2-des 3-CP 4-CF 5-CV 6-IV 7-ML
+        for i in range(len(lista)):
+            CP=lista[i][3]
+            CF=lista[i][4]
+            CV=lista[i][5]
+            IV=lista[i][6]
+            ML=lista[i][7]
+
+             #Calculo do preço de venda:
+            soma=CF+CV+IV+ML
+            if soma>100:
+                soma = soma * -1
+
+            PV = CP/(1-((soma)/100))
+            #################################
+
+            #Custo de aquisição
+            PCA=(CP*100)/PV
+            #Receita bruta:
+            RB=PV-CP
+            PRB=100-PCA
+            #ValorImposto
+            VI=(IV/100)*PV
+            #Comissão
+            VCV=(CV/100)*PV
+            #Rentabilidade
+            R=(ML/100)*PV
+            #Custofixo:
+            VCF=(CF/100)*PV
+            #OutrosCustos
+            OC=VCF+VCV+VI
+            POC=CF+CV+IV
+
+            #organização e impressão das tabelas
+            tabela = [
+                ["Descrição", "Valor", "%"],
+                ["A.Preço de Venda:", PV, "100%"],
+                ["B.Custo de Aquisição:", CP, f"{round(PCA)}%"],
+                ["C.Receita Bruta(A-B):", RB, f"{round(PRB)}%"],
+                ["D.Custo Fixo/Administrativo:", VCF, f"{round(CF)}%"],
+                ["E.Comissão de vendas:", VCV, f"{round(CV)}%"],
+                ["F.Impostos:", VI, f"{round(IV)}%"],
+                ["G.Outros Custos(D+E+F):", OC, f"{round(POC)}%"],
+                ["H.Rentabilidade(C-G):", R, f"{round(ML)}%"]
+            ]
+            cabeçalho=[[lista[i][0], lista[i][1], lista[i][2]]]
+            print(tabulate(cabeçalho, tablefmt="rounded_outline"))
+            print(tabulate(tabela, headers="firstrow", tablefmt="rounded_outline", floatfmt=".2f"))
+
+            #Classificação de lucro:
+            if ML>20:
+                print(tabulate([["Lucro alto"]], tablefmt="rounded_outline"))
+                print()
+            elif ML>10 and ML<=20:
+                print(tabulate([["Lucro médio"]], tablefmt="rounded_outline"))
+                print()
+            elif ML>0 and ML<=10:
+                print(tabulate([["Lucro baixo"]], tablefmt="rounded_outline"))
+                print()
+            elif ML==0:
+                print(tabulate([["Equilibro"]], tablefmt="rounded_outline"))
+                print()
+            elif ML<0:
+                print(tabulate([["Prejuízo"]], tablefmt="rounded_outline"))
+                print()
+            ######################################
+
+    if sel1 == 3:
+        #apagar dados
+        select = input("Digite:\n 1-Apagar todos os produtos\n 2-Apagar produto específico: ")
+        if select == '1':
+            cursor.execute("TRUNCATE TABLE Projeto_Integrador")
+        elif select == '2':
+            código = input("Digite o código do produto que deseja apagar: ")
+            deletar = "DELETE FROM Projeto_Integrador WHERE COD_PROD = :código"
+            cursor.execute(deletar, código=código)
+            connection.commit()
 
 
-cursor.close
-connection.close()
+
+
+    cursor.close
+    connection.close()
